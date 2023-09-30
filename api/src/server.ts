fd074c53b05e99d5e2a59d94b51b0aca7ea96952
ricardobizerra <ricardo.bizerra19@gmail.com>
2023-09-30 00:38:30 -0300

    feat(main): back-end setup

---
+++ api/src/server.ts
@@ -0,0 +1,13 @@
+import fastify from 'fastify';
+
+const app = fastify();
+
+app.listen({
+    port: 3333,
+})
+.then(() => {
+    console.log('🚀 Server running on http://localhost:3333');
+})
+.catch((err) => {
+    console.error('Ocorreu um erro ao iniciar o Back-End:', err);
+});
